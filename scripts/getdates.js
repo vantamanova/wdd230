e6c21492d9d22684320c2512c5a6889f2d0d084f
Vera Antamanova <vantamanova@yandex.ru>
2024-01-17 12:13:16 +0400

    scripts issue

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -13,5 +13,6 @@ const info = document.querySelector('#info');
 const year = new Date().toLocaleDateString("en-US", {year: "numeric"});
 
 info.insertAdjacentHTML('beforebegin', `<p> &copy ${year} </p>`);
-info.insertAdjacentHTML('afterend', '<p>UAE</p>');
+info.insertAdjacentHTML('afterend', '<p>UAE</p>'); 
+
 
