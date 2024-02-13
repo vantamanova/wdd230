a8fedd26389aae70986736046da7cc842a551bed
Vera Antamanova <vantamanova@yandex.ru>
2024-02-13 14:14:47 +0400

    dark mode

---
+++ chamber/scripts/darkmore.js
@@ -0,0 +1,8 @@
+const modeButton = document.querySelector(".slider");
+const body = document.querySelector("body");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	body.classList.toggle('dark-mode');
+	main.classList.toggle('dark-mode');
+});
