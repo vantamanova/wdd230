5469cf406e2ad6ffe1434edbde749a38eb185b3e
Vera Antamanova <vantamanova@yandex.ru>
2024-02-26 13:48:27 +0400

    06 Assignment: Chamber Home Responsive Design

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,10 +1,17 @@
-var temp= 51;
-var wSpeed= 2;
+const button = document.querySelector(".button")
 
-if ((temp <= 50) || (wSpeed > 3)) {
-    document.querySelector(".windChill").innerHTML= "N/A";
-} else {
-    var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
-    var windChill= Math.round(windChill);
-    document.querySelector(".windChill").innerHTML= windChill;
-}
+button.addEventListener("click", function() {
+    var temp = parseFloat(document.querySelector("#temperature").value);
+    var wSpeed = parseFloat(document.querySelector("#windspeed").value);
+    
+    console.log(temp);
+    console.log(wSpeed);
+
+    if ((temp <= 50) || (wSpeed < 3)) {
+        document.querySelector(".windChill").innerHTML= "N/A";
+    } else {
+        var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+        var windChill= Math.round(windChill);
+        document.querySelector(".windChill").innerHTML= windChill;
+    }
+});
