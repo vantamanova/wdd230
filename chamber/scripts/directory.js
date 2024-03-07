5a7f07722908374cc1cbcc4821288bab02800dbf
Vera Antamanova <vantamanova@yandex.ru>
2024-03-07 21:41:19 +0400

    Form pattern fixed

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -3,11 +3,44 @@ const directoryURL = "https://vantamanova.github.io/wdd230/chamber/data/members.
 
 const cards = document.querySelector("#cards");
 
-async function getLinks(directoryURL) {
+async function getData(directoryURL) {
     const response = await fetch(directoryURL);
     const data = await response.json();
 
-    displayLinks(data);
+    //console.log(data.companies);
+    displayData(data.companies);
 }
 
-getLinks(directoryURL);
+getData(directoryURL);
+
+function displayData(companies) {
+
+    companies.forEach(company => {
+        let card = document.createElement("section");
+        const logo = document.createElement("img");
+        const name = document.createElement("p");
+        const adress = document.createElement("p");
+        const phone = document.createElement("p");
+        const email = document.createElement("a");
+
+        name.textContent = company.name;
+        adress.textContent = company.adress;
+        phone.textContent = company.phone;
+        email.textContent = company.email;
+
+        logo.setAttribute('src', company.image);
+        logo.setAttribute('alt', `${company.name} logo`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '340');
+        logo.setAttribute('height', '440');
+
+        card.appendChild(name);
+        card.appendChild(logo);
+        card.appendChild(adress);
+        card.appendChild(phone);
+        card.appendChild(email);
+
+        cards.appendChild(card);
+    });
+
+}
