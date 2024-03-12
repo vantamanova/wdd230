377b61a8b01b9b9ccf6da86ff7baa1ccf7d52644
Vera Antamanova <vantamanova@yandex.ru>
2024-03-12 16:34:22 +0400

    Chamber Directory Page

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,7 +1,7 @@
 const baseURL = "https://vantamanova.github.io/wdd230/chamber/";
 const directoryURL = "https://vantamanova.github.io/wdd230/chamber/data/members.json";
 
-const cards = document.querySelector("#cards");
+const cards = document.querySelector(".cards");
 
 async function getData(directoryURL) {
     const response = await fetch(directoryURL);
@@ -17,22 +17,24 @@ function displayData(companies) {
 
     companies.forEach(company => {
         let card = document.createElement("section");
+        card.setAttribute("class", "company");
+
         const logo = document.createElement("img");
-        const name = document.createElement("p");
+        const name = document.createElement("h3");
         const adress = document.createElement("p");
         const phone = document.createElement("p");
         const email = document.createElement("a");
 
         name.textContent = company.name;
-        adress.textContent = company.adress;
+        adress.textContent = company.address;
         phone.textContent = company.phone;
-        email.textContent = company.email;
+        email.textContent = company.website;
 
-        logo.setAttribute('src', company.image);
+        logo.setAttribute('src', `images/${company.image}`);
         logo.setAttribute('alt', `${company.name} logo`);
         logo.setAttribute('loading', 'lazy');
-        logo.setAttribute('width', '340');
-        logo.setAttribute('height', '440');
+        logo.setAttribute('width', '100');
+        logo.setAttribute('height', '100');
 
         card.appendChild(name);
         card.appendChild(logo);
