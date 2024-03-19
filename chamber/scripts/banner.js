1c11cc56d3688f77540f6615b15d66a25afaa646
Vera Antamanova <vantamanova@yandex.ru>
2024-03-19 18:56:50 +0400

    close

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -5,3 +5,11 @@ const dateNow = new Date().getDay();
 if ((dateNow == 1) || (dateNow == 2) || (dateNow == 3)) {
     banner.classList.add("show");
 }
+
+const closeButton = document.querySelector('#close');
+
+
+closeButton.addEventListener('click', () => {
+	banner.classList.toggle('hide');
+	closeButton.classList.toggle('hide');
+});
